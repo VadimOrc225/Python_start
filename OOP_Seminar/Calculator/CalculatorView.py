a26a60903def3257f24044de3f8a179452274bc6
Vadim <orc225@mail.ru>
2023-07-19 22:12:33 +0300

    Initial commit for final task (attestation)

---
+++ OOP_Seminar/Calculator/CalculatorView.py
@@ -0,0 +1,10 @@
+# Представление (View) - отображает пользовательский интерфейс и обрабатывает ввод пользователя
+class CalculatorView:
+    def get_input(self):
+        num1 = float(input("Введите первое число: "))
+        operator = input("Введите оператор (+, -, *, /): ")
+        num2 = float(input("Введите второе число: "))
+        return num1, operator, num2
+
+    def display_result(self, result):
+        print("Результат:", result)
